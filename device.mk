3242b2d367cd6554ba3f93ec4fdb140624be6bd0
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-29 20:11:19 +0300

    cerro: include own Soong namespace
    Change-Id: Ie6464970d66bcb202dfe2c9019b7cb812dd569ea

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/display_id_4630946566460097411.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946566460097411.xml
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
 # Overlays
 PRODUCT_PACKAGES += \
     SettingsProviderResCerro
