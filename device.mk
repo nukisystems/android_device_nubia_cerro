0776521f1cffa00ae834075c2376ef13d532fc54
Giovanni Ricca <giovanniricca@proton.me>
2025-09-12 23:55:49 +0200

    cerro: Commonize displayconfig
    Change-Id: I2a2e69b6499b3575431a2c468ce9f393f6f34825

--- device.mk
+++ device.mk
@@ -12,13 +12,6 @@ DEVICE_PATH := device/nubia/cerro
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/mixer_paths_pineapple_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple/mixer_paths_pineapple_mtp.xml
 
-# Display
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/display_id_4630946566460097411.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946566460097411.xml
-
-# Fingerprint
-TARGET_HAS_UDFPS := true
-
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
